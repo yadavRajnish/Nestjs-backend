42de6e07e0e48b4caace3ab869703fe2a9ed3b63
Rajnish Yadav <rajnishyadav5509@gmail.com>
2024-10-11 18:22:28 +0530

    Feature : Add the prisma

---
+++ sample.env.ts
@@ -0,0 +1,12 @@
+// DB_PORT = ''
+// DB_USERNAME = ''
+// DB_PASSWORD = ''
+// DB_HOST = ''
+// DB_DATABASE = ''
+// DEVELOPMENT_API_URL = http://localhost:3000
+// # NODE_ENV = development
+// # JWT_SECRET =
+
+// # DATABASE_URL = mysql:
+
+// ${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}
